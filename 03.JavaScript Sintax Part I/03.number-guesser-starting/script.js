4ac46f20c58debc19ac01064c2d637600d719ab7
jfabriciovr <fabricio33@gmail.com>
2025-10-31 16:29:14 -0600

    Implement core game logic for number guessing game

--- 03.JavaScript Sintax Part I/03.number-guesser-starting/script.js
+++ 03.JavaScript Sintax Part I/03.number-guesser-starting/script.js
@@ -4,3 +4,35 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+//returns a random integer between 0 and 9
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+const getAbsoluteDistance = (targetNumber, guessNumber) =>
+  Math.abs(targetNumber - guessNumber);
+
+const compareGuesses = (userGuess, computerGuess, secretTargetNum) => {
+  if (!(userGuess >= 0 && userGuess <= 9)) {
+    alert("number has to be between 0 and 9");
+    return false;
+  } else {
+    const userDifference = getAbsoluteDistance(secretTargetNum, userGuess);
+    const computerDifference = getAbsoluteDistance(
+      secretTargetNum,
+      computerGuess
+    );
+    /*
+  if (userDifference <= computerDifference)
+    return true;
+  else
+    return false;
+    */
+    return userDifference <= computerDifference;
+  }
+};
+
+const updateScore = (winner) => {
+  if (winner === "human") humanScore++;
+  else if (winner === "computer") computerScore++;
+};
+
+const advanceRound = () => currentRoundNumber++;
