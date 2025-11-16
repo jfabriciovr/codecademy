427bef2d3c3252dc2fdc370b6c024cf6b89b7a2d
jfabriciovr <fabricio33@gmail.com>
2025-11-15 23:36:33 -0600

    Update script.js

--- 03.JavaScript Sintax Part I/04.mixed-messages/script.js
+++ 03.JavaScript Sintax Part I/04.mixed-messages/script.js
@@ -2,8 +2,20 @@ generateWisdom();
 
 function generateWisdom() {
   let personalWisdom = [];
+  const wisdom = {
+    planet: ["moon", "sun", "mars"],
+    luck: ["good luck", "bad luck"],
+    when: ["this year", "next year"],
+  };
+
+  for (let item in wisdom) {
+    personalWisdom.push(item[1][1]);
+
+  }
+  
+  console.log(personalWisdom);
 }
 
 function generateRandomNumber(num) {
-  return Math.floor(Math.random() * num);
+  return Math.floor(Math.random() * num); //from 0 to num -1
 }
