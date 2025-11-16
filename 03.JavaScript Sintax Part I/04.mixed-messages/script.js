49347bf632ac3de64ecdb403e35d31c1b62912c3
jfabriciovr <fabricio33@gmail.com>
2025-11-15 23:57:05 -0600

    Refactor wisdom generation logic to use a collectiveWisdom object and improve random selection

--- 03.JavaScript Sintax Part I/04.mixed-messages/script.js
+++ 03.JavaScript Sintax Part I/04.mixed-messages/script.js
@@ -1,21 +1,28 @@
-generateWisdom();
-
-function generateWisdom() {
-  let personalWisdom = [];
-  const wisdom = {
-    planet: ["moon", "sun", "mars"],
-    luck: ["good luck", "bad luck"],
-    when: ["this year", "next year"],
-  };
-
-  for (let item in wisdom) {
-    personalWisdom.push(item[1][1]);
-
-  }
-  
-  console.log(personalWisdom);
+function generateRandomNumber(num) {
+  //Gets # from 0 -> num - 1
+  return Math.floor(Math.random() * num);
 }
 
-function generateRandomNumber(num) {
-  return Math.floor(Math.random() * num); //from 0 to num -1
+// Define an object with all the possibilities to choose randomly
+const collectiveWisdom = {
+  signInfo: ["star", "moon", "sun", "comet"],
+  fortuneOutput: ["terrible luck", "bad luck", "ok luck", "good luck", "great luck"],
+  advice: ["go out and eat", "not read this", "play more", "trust no one"],
+};
+// Store the 'wisdom' in an array
+// This is what will be outputed in the terminal
+let personalWisdom = [];
+
+// Iterate over the object
+for (let prop in collectiveWisdom) {
+  // generates a random number between the elements of the arrays
+  let optionIdx = generateRandomNumber(collectiveWisdom[prop].length);
+
+  //use object's properties to customize the message being added to personalWisdom
+  switch(prop) {
+    case "signInfo":
+      personalWisdom.push
+  }
+  console.log(collectiveWisdom[prop][optionIdx]);
+
 }
